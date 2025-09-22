28437471630a9364d0ce7f781f1af0566b46e60b
Niko <ster_vet@hotmail.com>
2025-09-23 00:34:47 +0200

    Build Applayout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Test</p>
+    </header>
+  );
+}
+
+export default Header;
